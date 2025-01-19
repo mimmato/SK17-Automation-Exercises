ad60cdf9a5a730c44b3db61762a6b2eb1a18abc3
mimmato <mimmato@gmail.com>
2025-01-19 19:13:27 +0200

    finished e5 of l8 and made corrections to logic

--- src/lesson8/l8_e5_secondLargestNumber.java
+++ src/lesson8/l8_e5_secondLargestNumber.java
@@ -2,18 +2,36 @@ package lesson8;
 
 public class l8_e5_secondLargestNumber {
     public static void main(String[] args) {
-        System.out.println("This number is the seconds largest from the array: ");
-        secondLargest();
+        System.out.println("The second biggest number from the array is: ");
+
+        //        secondLargestFrom0();
+
+        secondLargestFromArrayLength();
+    }
+    static int[] numbers = {17, 11, 23, 10, 20, 30, 40, 40, 50, 50, 20, 30};
+
+    private static void secondLargestFromArrayLength() {
+        int biggestNumber = numbers[0];
+        int secondBiggest = numbers[0];
+
+        for(int number : numbers) {
+
+            if (biggestNumber < number){
+                secondBiggest = biggestNumber;
+                biggestNumber = number;
+            } else if (number > secondBiggest && number < biggestNumber ){
+                secondBiggest = number;
+            }
+        }
+        System.out.println(secondBiggest);
     }
 
-    static int[] arrayList = {17, 11, 23, 10, 20, 30, 40, 50};
-
-    private static void secondLargest() {
+    private static void secondLargestFrom0() {
 
         int secondBigger = 0;
         boolean isSecondBigger = false;
 
-            for (int number : arrayList) {
+            for (int number : numbers) {
                 if (secondBigger < number) {
                     secondBigger = number;
 
