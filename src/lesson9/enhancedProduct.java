cc6fdbf374818b10e40af6c9b4d3ff5da11a2ea9
mimmato <mimmato@gmail.com>
2025-01-26 20:41:59 +0200

    finished e4 of l9

---
+++ src/lesson9/enhancedProduct.java
@@ -0,0 +1,45 @@
+package lesson9;
+
+public class enhancedProduct {
+
+    public String productName;
+    public double price;
+    public String category;
+    public int quantity;
+    public boolean saleStatus;
+
+    static String categoryOnSale = "fruits";
+
+    public enhancedProduct(String productName, double price, String category, int quantity, boolean saleStatus){
+
+        this.productName = productName;
+        this.price = price;
+        this.category = category;
+        this.quantity = quantity;
+
+        this.saleStatus = this.category.equals(categoryOnSale);
+    }
+
+    public double discountPrice() {
+      double discount = 0.10;
+      double newPrice = this.price;
+
+      if (this.saleStatus && newPrice > 100){
+          double discountAmount = price * discount;
+          newPrice = price - discountAmount;
+      }
+
+      return newPrice;
+
+    }
+
+    public void printProductList(){
+        System.out.println("Product name: " + this.productName);
+        System.out.println("Category is: " + this.category);
+        System.out.println("Current product price: " + this.price);
+        System.out.println("New product price if discounted: " + this.discountPrice());
+
+        System.out.println("Sale status: " + this.saleStatus);
+    }
+
+}
